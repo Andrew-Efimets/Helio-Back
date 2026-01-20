176d6a00045811a0d459d3b94c5ce710a7b02e69
Andrew Efimets <mynewspecialpost@gmail.com>
2026-01-20 20:52:27 +0300

    First API response

--- laravel/app/Models/Message.php
+++ laravel/app/Models/Message.php
@@ -1,10 +1,40 @@
 <?php
+//
+//namespace App\Models;
+//
+//use Illuminate\Database\Eloquent\Model;
+//
+//class Message extends Model
+//{
+//    //
+//}
+
 
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
-class Message extends Model
+class Photos extends Model
 {
-    //
+    protected $fillable = [
+        'filename',
+        'user_id',
+    ];
+
+    protected $appends = ['url'];
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function getUrlAttribute()
+    {
+        if (!$this->filename) {
+            return null;
+        }
+
+        return Storage::disk('s3')->url($this->filename);
+    }
 }
--- laravel/routes/api.php
+++ laravel/routes/api.php
@@ -8,6 +8,26 @@ use Illuminate\Support\Facades\Route;
 //    return $request->user();
 //})->middleware('auth:sanctum');
 
-Route::prefix('v1')->group(function () {
-    Route::apiResource('user', UserController::class);
+//Route::get('test', function () {
+//    return response()->json([
+//        'message' => 'Your test'
+//    ]);
+//});
+
+Route::get('/login', function () {
+    return 'Unauthorize';
+})->name('login');
+
+Route::prefix('/v1')->group(function () {
+    Route::post('/user', [UserController::class, 'store']);
+    Route::get('/users', [UserController::class, 'index'])
+        ->middleware('auth:sanctum');
+    Route::get('/user/{id}', [UserController::class, 'show'])
+        ->middleware('auth:sanctum');
+    Route::patch('/user/{id}', [UserController::class, 'update'])
+        ->middleware('auth:sanctum');
+    Route::delete('/user/{id}', [UserController::class, 'destroy'])
+        ->middleware('auth:sanctum');
 });
+
+
